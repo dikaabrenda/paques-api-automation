d4031752ad1e0521e8e6cea4bf1fb81d2d709bd1
Dika Brenda <dika.brenda@paques.id>
2022-06-08 13:15:58 +0700

    test ci

--- src/test/credentials/login.spec.js
+++ src/test/credentials/login.spec.js
@@ -4,9 +4,18 @@ import auth from '../../routes/auth.api';
 import * as data from '../../data/auth/auththorization.data';
 
 describe('as a user, i want to logged in PDS Application', () => {
-  it('Client send a POST request "/api/login"', async () => {
-    const res = await auth.POST(data.Credentials);
-    assert.equal(res.status, 200);
-    localStorage.setItem('token', res.data.response.access_token)
+  it.only('Client send a POST request "/api/login"', async () => {
+    await auth.POST(data.Credentials)
+      .then((res) => {
+        const prefix = res.data.response.profile;
+        assert.equal(res.status, 200);
+        assert.equal(prefix.username, 'dikabrenda');
+        assert.equal(prefix.email, 'dika.brenda@paques.id');
+        assert.isNotNull(prefix.access_token);
+        localStorage.setItem('token', res.data.response.access_token);
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
   });
 });
