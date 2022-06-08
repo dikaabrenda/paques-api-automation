ff45a6da799732da2b14b22256a3f5eeabb1dad8
Dika Brenda <dika.brenda@paques.id>
2022-06-08 13:19:39 +0700

    adjust scenario login

--- src/test/credentials/login.spec.js
+++ src/test/credentials/login.spec.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import { describe, it } from 'mocha';
-import auth from '../../routes/auth.api';
-import * as data from '../../data/auth/auththorization.data';
+import auth from '../../routes/auth.api.js';
+import * as data from '../../data/auth/auththorization.data.js';
 
 describe('as a user, i want to logged in PDS Application', () => {
   it.only('Client send a POST request "/api/login"', async () => {
