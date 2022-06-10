bd94f6131e0582ab4639107b176b06039429fab6
Dika Brenda <dika.brenda@paques.id>
2022-06-10 11:15:18 +0700

    adjust test scenario endpoint CRUD user

--- src/test/pds/UserManagement/users/UpdateUsers.spec.js
+++ src/test/pds/UserManagement/users/UpdateUsers.spec.js
@@ -1,15 +1,20 @@
 import chai, { assert } from 'chai';
-import chaiJsonSchema from 'chai-json-schema';
 import users from '../../../../routes/users.api';
+import chaiJsonSchema from 'chai-json-schema';
 import * as data from '../../../../data/users/UpdateUsers.data';
+import * as schema from '../../../../schema/pds/users/update.user.schema';
 
 chai.use(chaiJsonSchema);
 
 describe('as a client, i want to update an user in PDS Application', () => {
-  it('client send a PUT request "api/user/:id"', async () => {
+  it('Client send a PUT request "api/user/:id"', async () => {
     await users.PUT(data.UPDATE_USERS)
       .then((res) => {
         assert.equal(res.status, 200);
+        assert.equal(res.data.response.email, 'automation_test@mailnesia.com');
+        assert.equal(res.data.response.firstname, 'Automation');
+        assert.equal(res.data.response.lastname, 'Test');
+        assert.jsonSchema(res.data, schema.UPDATE_USER);
       })
       .catch((err) => {
         throw new Error(err.message);
